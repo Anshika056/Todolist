de6c22c4ca49c0be190703d5f21fe0fe93fbdbee
anshika056 <anshikamadan2@gmail.com>
2022-02-02 02:28:43 +0530

    added header

---
+++ blog-frontend/src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../../components/header(banner)/Header';
+import "./home.css"
+
+export default function Home() {
+  return (
+  <div className='home'>
+    <Header />
+  </div>)
+}
